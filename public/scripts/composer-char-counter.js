4ad4eab6703c9f274d34be8a1d8e0694c696dda9
JayShin <08jhs05@gmail.com>
2021-04-20 16:42:21 -0400

    add hander to show remaining allowed characters count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
   console.log('dom loaded')
-});
 
-$("#tweet-text").on("keypress", function() {
-  console.log($(this)["0"].value)
-  $("output.counter")["0"].value = $(this)["0"].value.length;
-})
+  $("#tweet-text").on("input", function() {
+    $("output.counter")["0"].value = 140 - $(this)["0"].value.length;     // set value of text counter - shows remaining allowed characters count
+
+    if( $("output.counter")["0"].value < 0 ) {         // checks if the counter goes below 0
+      $("output.counter").css("color", "red");        // if it does, change color to red, black otherwise
+    } else $("output.counter").css("color", "black");
+  });
+
+});
