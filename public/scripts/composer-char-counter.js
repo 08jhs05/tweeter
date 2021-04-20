b1665b388270facd169c70e262925b7a46ec113b
JayShin <08jhs05@gmail.com>
2021-04-20 16:59:57 -0400

    update to show correcte count and color when page is refreshed

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
-  console.log('dom loaded')
+  console.log('dom loaded');
+  
+  setCounterAndChangeColor("#tweet-text");
 
   $("#tweet-text").on("input", function() {
-    $("output.counter")["0"].value = 140 - $(this)["0"].value.length;     // set value of text counter - shows remaining allowed characters count
-
-    if( $("output.counter")["0"].value < 0 ) {         // checks if the counter goes below 0
-      $("output.counter").css("color", "red");        // if it does, change color to red, black otherwise
-    } else $("output.counter").css("color", "black");
+    setCounterAndChangeColor(this);
   });
 
 });
+
+const setCounterAndChangeColor = function(tweetTextDom) {
+  $("output.counter")["0"].value = 140 - $(tweetTextDom)["0"].value.length;     // set value of text counter - shows remaining allowed characters count
+  if( $("output.counter")["0"].value < 0 ) {                                 // checks if the counter goes below 0
+    $("output.counter").css("color", "red");                                 // if it does, change color to red, black otherwise
+  } else $("output.counter").css("color", "black");
+}
