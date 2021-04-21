7b0ee926da1ddb6ec483c704917cb2adc86c7625
JayShin <08jhs05@gmail.com>
2021-04-21 18:33:17 -0400

    tweet-container automatically updates when new tweet is posted

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,4 +1,7 @@
 const renderTweets = function(tweets) {
+  tweets.sort((a, b) => {
+    return a.created_at < b.created_at;             // sort tweets by comparing its time created - tweet created most recently comes first
+  })
   for (const tweet of tweets) {
     const newTweet = createTweetElement(tweet);     // create a new tweet with passed data
     $(".tweet-container").append(newTweet)          // and append it at tweets container
@@ -6,27 +9,28 @@ const renderTweets = function(tweets) {
 }
 
 const createTweetElement = function(tweet) {        // create a single tweet article from passed data
-let $tweet = `<article class="tweet-article">
-<header class="tweet-header">
-  <img src=${tweet.user.avatars}>
-  <div class="tweetUserName">${tweet.user.name}</div>
-  <div class="tweetAddress">${tweet.user.handle}</div>
-</header>
-  <p class="tweet-body">${tweet.content.text}</p>
-  <hr>
-<footer class="tweet-footer">
-  <div id="tweet-time">${timeago.format(tweet.created_at)}</div>
-  <div class="tweet-icons">
-    <i class="fas fa-flag"></i>
-    <i class="fas fa-retweet"></i>
-    <i class="fas fa-heart"></i>
-  </div>
-</footer>
-</article>`
-return $tweet;
+  let $tweet = `<article class="tweet-article">
+    <header class="tweet-header">
+      <img src=${tweet.user.avatars}>
+      <div class="tweetUserName">${tweet.user.name}</div>
+      <div class="tweetAddress">${tweet.user.handle}</div>
+    </header>
+      <p class="tweet-body">${tweet.content.text}</p>
+      <hr>
+    <footer class="tweet-footer">
+      <div id="tweet-time">${timeago.format(tweet.created_at)}</div>
+      <div class="tweet-icons">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </footer>
+    </article>`
+  return $tweet;
 }
 
 const loadTweets = function() {           //load tweets with ajax GET request to /tweets
+  $(".tweet-container").empty();          // reset tweet container
   $.ajax({
     url: "tweets",
     type: "get"
@@ -50,7 +54,7 @@ $('.new-tweet-form').on('submit', (event) => {   //post new tweet with ajax POST
     url: "tweets",
     type: "post",
     data: str
-  });
+  }).done(loadTweets);              //load tweets again when request is finished
 });
 
 loadTweets();
