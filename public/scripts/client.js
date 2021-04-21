a87844d0669979228c5c10ffafbfd3c80effeefa
JayShin <08jhs05@gmail.com>
2021-04-21 17:29:11 -0400

    add loadTweets function to request Ajax GET to fetch tweet data from server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,29 +1,3 @@
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const renderTweets = function(tweets) {
   for (const tweet of tweets) {
     const newTweet = createTweetElement(tweet);     // create a new tweet with passed data
@@ -52,4 +26,23 @@ let $tweet = `<article class="tweet-article">
 return $tweet;
 }
 
-renderTweets(data);
+const loadTweets = function() {           //load tweets with ajax GET request to /tweets
+  $.ajax({
+    url: "tweets",
+    type: "get"
+  }).then(function(response) {
+    renderTweets(response);
+  })
+}
+
+$('.new-tweet-form').on('submit', (event) => {   //post new tweet with ajax POST request
+  event.preventDefault();
+  let str = $("#tweet-text").serialize();
+  $.ajax({
+    url: "tweets",
+    type: "post",
+    data: str
+  });
+});
+
+loadTweets();
